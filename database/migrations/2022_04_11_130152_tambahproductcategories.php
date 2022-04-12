ae8d99adc8e8fdf66afbd94bba025416d954e8aa
mindatama <mindatama@gmail.com>
2022-04-12 12:57:05 +0800

    edit migrations product&productcategories

--- database/migrations/2022_04_11_130152_tambahproductcategories.php
+++ database/migrations/2022_04_11_130152_tambahproductcategories.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2022_04_11_130351_tambahproduct.php
+++ database/migrations/2022_04_11_130351_tambahproduct.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->longText('description')->nullable();
+            $table->string('tags')->nullable();
+            $table->bigInteger('categories_id');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
