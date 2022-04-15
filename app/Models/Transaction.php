d84996692bbd5e0362fb299438e8c49306a3bae6
mindatama <mindatama@gmail.com>
2022-04-16 06:06:57 +0800

    isi model transaction&prodgal

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'users_id',
+        'address',
+        'payment',
+        'total_price',
+        'shipping_price',
+        'status',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+    public function items(){
+        return $this->hasMany(TransactionItem::class, 'transactions_id', 'id');
+    }
 }
