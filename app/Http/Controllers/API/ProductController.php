928b529c8d56cb774c78a05ba3b1bba990a0c135
mindatama <mindatama@gmail.com>
2022-04-22 21:53:57 +0800

    product API 2

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function all(Request $request){}
+    public function all(Request $request){
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $tags = $request->input('tags');
+        $categories = $request->input('categories');
+
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+    }
 }
