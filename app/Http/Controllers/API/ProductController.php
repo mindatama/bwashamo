061b81f06a977ae60a5a730a5234fcb9408f0726
mindatama <mindatama@gmail.com>
2022-04-23 12:48:58 +0800

    product API 2

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -17,5 +17,45 @@ class ProductController extends Controller
 
         $price_from = $request->input('price_from');
         $price_to = $request->input('price_to');
+
+        if ($id){
+            $product = Product::with(['category','galleries'])->find($id);
+
+            if($product)
+                return ResponseFormatter::success(
+                    $product,
+                    'Data produk berhasil diambil'
+                );
+            else
+                return ResponseFormatter::error(
+                    null,
+                    'Data produk tidak ada',
+                    404
+                );
+        }
+        $product = Product::with(['category','galleries']);
+
+        if($name)
+            $product->where('name', 'like', '%' . $name . '%');
+
+        if($description)
+            $product->where('description', 'like', '%' . $description . '%');
+
+        if($tags)
+            $product->where('tags', 'like', '%' . $tags . '%');
+
+        if($price_from)
+            $product->where('price', '>=', $price_from);
+
+        if($price_to)
+            $product->where('price', '<=', $price_to);
+
+        if($categories)
+            $product->where('categories_id', $categories);
+
+        return ResponseFormatter::success(
+            $product->paginate($limit),
+            'Data list produk berhasil diambil'
+        );
     }
 }
